cd8f436df537efb87ee874df91e8eba43db9f492
sergey.ignatievskiy <sergey.ignatievskiy@cfd9bf1c-2418-e9a9-f1a6-b781b36a0197>
2010-12-20 21:12:44 +0000

    Версия 1.0.0

---
+++ Labyrinthus/Pages/PagePreparePrimitives.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Labyrinthus.Pages
+{
+  /// <summary>
+  /// Interaction logic for PagePreparePrimitives.xaml
+  /// </summary>
+  public partial class PagePreparePrimitives
+  {
+
+
+    #region Конструктор
+
+    public PagePreparePrimitives()
+    {
+      InitializeComponent();
+    }
+
+    #endregion
+
+    #region Загрузка и сохранение
+
+    private void LoadPrimitive(object sender, RoutedEventArgs e)
+    {
+      MessageBox.Show("Тут будет загрузка примитива из файла");
+    }
+
+    private void SavePrimitive(object sender, RoutedEventArgs e)
+    {
+      MessageBox.Show("Тут будет сохранение примитива в файл");
+    }
+
+    #endregion
+
+    protected void PrimitivePropertiesChanged()
+    {
+      int w, h;
+      if (Int32.TryParse(PrimitiveWidth.Text, out w) && Int32.TryParse(PrimitiveHeight.Text, out h))
+      {
+        if (h > 0 && w > 0)
+        {
+          PrimitiveCanvas.SetNewSizes(h, w);
+        }
+      }
+    }
+
+    private void Primitive_TextChanged(object sender, TextChangedEventArgs e)
+    {
+      PrimitivePropertiesChanged();
+    }
+  }
+}
