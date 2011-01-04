17327b46524bb64026f5531a9292217fe3108061
arpmipg <arpmipg@cfd9bf1c-2418-e9a9-f1a6-b781b36a0197>
2011-01-04 21:30:25 +0000

    Issue 11: 3D отображение - добавлено приближение и удаление

--- trunk/Labyrinthus/Pages/PageShow3DLabyrinthus.xaml.cs
+++ trunk/Labyrinthus/Pages/PageShow3DLabyrinthus.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Media3D;
 using Labyrinthus.Objects;
@@ -15,6 +17,15 @@ namespace Labyrinthus.Pages
     #endregion
 
     #region Поля
+    /// <summary>
+    /// Камера
+    /// </summary>
+    private PerspectiveCamera labirunthusCamera;
+    /// <summary>
+    /// Материал для пола
+    /// </summary>
+    private readonly DiffuseMaterial floorMaterial;
+
     /// <summary>
     /// Материал для стен лабиринта
     /// </summary>
@@ -43,9 +54,10 @@ namespace Labyrinthus.Pages
     {
       InitializeComponent();
 
-      EdgeWidth = 2;
-      EdgeHeight = 2;
+      EdgeWidth = 1;
+      EdgeHeight = 4;
 
+      floorMaterial = new DiffuseMaterial(new SolidColorBrush(Colors.Red));
       primitiveEdgeMaterial = new DiffuseMaterial(new SolidColorBrush(Colors.White));
       debugMaterial = new DiffuseMaterial(new SolidColorBrush(Colors.Green));
     }
@@ -55,6 +67,34 @@ namespace Labyrinthus.Pages
     private void Page_Loaded(object sender, RoutedEventArgs e)
     {
       DrawLabyrinthus();
+    }
+
+
+    private void LabyrinthusViewport_KeyDown(object sender, KeyEventArgs e)
+    {
+      var pos = labirunthusCamera.Position;
+      var look = labirunthusCamera.LookDirection;
+      var maxLook = Math.Max(Math.Max(look.X, look.Y), look.Z);
+      Point3D newPos;
+
+      switch (e.Key)
+      {
+        case Key.Up:
+          newPos = new Point3D(pos.X + look.X / maxLook,
+                               pos.Y + look.Y / maxLook,
+                               pos.Z + look.Z / maxLook);
+          break;
+        case Key.Down:
+          newPos = new Point3D(pos.X - look.X / maxLook,
+                               pos.Y - look.Y / maxLook,
+                               pos.Z - look.Z / maxLook);
+          break;
+        default:
+          newPos = new Point3D(pos.X, pos.Y, pos.Z);
+          break;
+      }
+
+      labirunthusCamera.Position = newPos;
     }
     #endregion
 
@@ -67,44 +107,110 @@ namespace Labyrinthus.Pages
       var labyrinthusModelGroup = new Model3DGroup();
       var labyrinthusGeometryModel = new GeometryModel3D();
 
-      var myDirectionalLight = new DirectionalLight
+      AddCamera();
+      AddLight(labyrinthusModelGroup);
+      DrawFloor(labyrinthusModelGroup);
+
+      for (int x = -3; x < 4; x++)
+      {
+        for (int y = -3; y < 4; y++)
+        {
+          DrawPrimitive(labyrinthusModelGroup, x, y);
+        }
+      }
+
+      labyrinthusModelGroup.Children.Add(labyrinthusGeometryModel);
+      labyrinthusModelVisual.Content = labyrinthusModelGroup;
+      LabyrinthusViewport.Children.Add(labyrinthusModelVisual);
+
+      labyrinthusGrid.Focus();
+    }
+
+
+    private void AddCamera()
+    {
+      labirunthusCamera = new PerspectiveCamera
+      {
+        Position = new Point3D(-3, -3, 5),
+        LookDirection = new Vector3D(1, 1.2, -1),
+        UpDirection = new Vector3D(0, 0, 1)
+      };
+
+      LabyrinthusViewport.Camera = labirunthusCamera;
+    }
+
+
+    private static void AddLight(Model3DGroup labyrinthusModelGroup)
+    {
+      var labyrinthusLight = new DirectionalLight
       {
         Color = Colors.White,
         Direction = new Vector3D(2, 5, -3)
       };
 
-      labyrinthusModelGroup.Children.Add(myDirectionalLight);
-
-
-      DrawPrimitive(labyrinthusModelGroup);
-
-      labyrinthusModelGroup.Children.Add(labyrinthusGeometryModel);
-      labyrinthusModelVisual.Content = labyrinthusModelGroup;
-      LabyrinthusViewport.Children.Add(labyrinthusModelVisual);
+      labyrinthusModelGroup.Children.Add(labyrinthusLight);
     }
 
 
-    private void DrawPrimitive(Model3DGroup labyrinthusModelGroup)
+    private void DrawFloor(Model3DGroup labyrinthusModelGroup)
+    {
+      // TODO: сделать нормальный пол
+      var floorPositions = new Point3DCollection
+      {
+        new Point3D(-100, -100, 0),
+        new Point3D( 100, -100, 0),
+        new Point3D(-100,  100, 0),
+        new Point3D( 100, -100, 0),
+        new Point3D(-100,  100, 0),
+        new Point3D( 100,  100, 0),
+      };
+
+      var floorIndices = new Int32Collection
+      {
+        0,1,2,
+        3,5,4
+      };
+
+      var floorGeometry = new MeshGeometry3D
+      {
+        Positions = floorPositions,
+        TriangleIndices = floorIndices
+      };
+      var floorGeometryModel = new GeometryModel3D
+       {
+         Geometry = floorGeometry,
+         Material = floorMaterial
+       };
+
+      labyrinthusModelGroup.Children.Add(floorGeometryModel);
+    }
+
+
+    private void DrawPrimitive(Model3DGroup labyrinthusModelGroup, int startX, int startY)
     {
       var wnd = (WindowMaster)Application.Current.MainWindow;
 
       foreach (var lineInfo in wnd.Primitive.Lines)
       {
-        var primitiveEdge = DrawPrimitiveEdge(lineInfo);
+        var primitiveEdge = DrawPrimitiveEdge(lineInfo, startX, startY);
 
         labyrinthusModelGroup.Children.Add(primitiveEdge);
       }
     }
 
-    private GeometryModel3D DrawPrimitiveEdge(LineInfo primitiveEdgeLine)
+
+    private GeometryModel3D DrawPrimitiveEdge(LineInfo primitiveEdgeLine, int startX, int startY)
     {
       // 2D координаты основания ребра примитива
       double x00, x01, x10, x11, y00, y01, y10, y11;
+      var wnd = (WindowMaster)Application.Current.MainWindow;
+      var primitiveWidth = wnd.Primitive.Width;
+      var primitiveHeight = wnd.Primitive.Height;
 
-      x00 = x01 = primitiveEdgeLine.X0 * ZOOM;
-      x10 = x11 = primitiveEdgeLine.X1 * ZOOM;
-      y00 = y01 = primitiveEdgeLine.Y0 * ZOOM;
-      y10 = y11 = primitiveEdgeLine.Y1 * ZOOM;
+      x00 = x01 = (primitiveEdgeLine.X0 + primitiveWidth * startX) * ZOOM;
+      x10 = x11 = (primitiveEdgeLine.X1 + primitiveWidth * startX) * ZOOM;
+      y00 = y01 = (primitiveEdgeLine.Y0 + primitiveHeight * startY) * ZOOM;
+      y10 = y11 = (primitiveEdgeLine.Y1 + primitiveHeight * startY) * ZOOM;
 
       if (primitiveEdgeLine.X0 == primitiveEdgeLine.X1)
       {
@@ -170,30 +276,26 @@ namespace Labyrinthus.Pages
       };
       primitiveEdgeGeometry.Positions = primitiveEdgePositions;
 
-      // TODO: поменять ориентацию для горизонтальных ребер
-      var primitiveEdgeIndices = new Int32Collection
-      {
-        0,1,2,    3,5,4,
-        6,8,7,    9,10,11,
-        12,13,14, 15,17,16,
-        18,20,19, 21,22,23,
-        24,25,26, 27,29,28,
-      };
+      // ориентация разная для горизонтальных и вертикальных ребер
+      var primitiveEdgeIndices = primitiveEdgeLine.X0 == primitiveEdgeLine.X1 ?
+        new Int32Collection
+        {
+          0,1,2,    3,5,4,
+          6,8,7,    9,10,11,
+          12,13,14, 15,17,16,
+          18,20,19, 21,22,23,
+          24,25,26, 27,29,28,
+        } :
+        new Int32Collection
+        {
+          0,2,1,    3,4,5,
+          6,7,8,    9,11,10,
+          12,14,13, 15,16,17,
+          18,19,20, 21,23,22,
+          24,26,25, 27,28,29,
+        };
 
       primitiveEdgeGeometry.TriangleIndices = primitiveEdgeIndices;
-
-//      var myNormalCollection = new Vector3DCollection();
-//      myNormalCollection.Add(new Vector3D(0, -1, 0));
-//      myNormalCollection.Add(new Vector3D(0, -1, 0));
-//      myNormalCollection.Add(new Vector3D(0, 1, 0));
-//      myNormalCollection.Add(new Vector3D(0, 1, 0));
-//      myNormalCollection.Add(new Vector3D(-1, 0, 0));
-//      myNormalCollection.Add(new Vector3D(-1, 0, 0));
-//      myNormalCollection.Add(new Vector3D(1, 0, 0));
-//      myNormalCollection.Add(new Vector3D(1, 0, 0));
-//      myNormalCollection.Add(new Vector3D(0, 0, 1));
-//      myNormalCollection.Add(new Vector3D(0, 0, 1));
-//      primitiveEdgeGeometry.Normals = myNormalCollection;
 
       var labyrinthusGeometryModel = new GeometryModel3D
        {
