2551bacc95b99ba270ade324275f71887bdb74c6
arpmipg <arpmipg@cfd9bf1c-2418-e9a9-f1a6-b781b36a0197>
2011-01-05 23:38:46 +0000

    Issue 13: Лабиринт перевернут в 3D

--- Labyrinthus/Pages/PageShow3DLabyrinthus.xaml.cs
+++ Labyrinthus/Pages/PageShow3DLabyrinthus.xaml.cs
@@ -328,8 +328,8 @@ namespace Labyrinthus.Pages
 
       x00 = x01 = (primitiveEdgeLine.X0 + primitiveWidth * startX) * ZOOM;
       x10 = x11 = (primitiveEdgeLine.X1 + primitiveWidth * startX) * ZOOM;
-      y00 = y01 = (primitiveEdgeLine.Y0 + primitiveHeight * startY) * ZOOM;
-      y10 = y11 = (primitiveEdgeLine.Y1 + primitiveHeight * startY) * ZOOM;
+      y00 = y01 = (- primitiveEdgeLine.Y0 + primitiveHeight * startY) * ZOOM;
+      y10 = y11 = (- primitiveEdgeLine.Y1 + primitiveHeight * startY) * ZOOM;
 
       if (primitiveEdgeLine.X0 == primitiveEdgeLine.X1)
       {
@@ -395,24 +395,14 @@ namespace Labyrinthus.Pages
       };
       primitiveEdgeGeometry.Positions = primitiveEdgePositions;
 
-      // ориентация разная для горизонтальных и вертикальных ребер
-      var primitiveEdgeIndices = primitiveEdgeLine.X0 == primitiveEdgeLine.X1 ?
-        new Int32Collection
-        {
-          0,1,2,    3,5,4,
-          6,8,7,    9,10,11,
-          12,13,14, 15,17,16,
-          18,20,19, 21,22,23,
-          24,25,26, 27,29,28,
-        } :
-        new Int32Collection
-        {
-          0,2,1,    3,4,5,
-          6,7,8,    9,11,10,
-          12,14,13, 15,16,17,
-          18,19,20, 21,23,22,
-          24,26,25, 27,28,29,
-        };
+      var primitiveEdgeIndices = new Int32Collection
+      {
+        0,2,1,    3,4,5,
+        6,7,8,    9,11,10,
+        12,14,13, 15,16,17,
+        18,19,20, 21,23,22,
+        24,26,25, 27,28,29,
+      };
 
       primitiveEdgeGeometry.TriangleIndices = primitiveEdgeIndices;
 
